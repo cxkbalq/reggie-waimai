4efaf4216be6dea09ef2f30e7bddf950d1bbffc0
cxkbalq <1955196623@qq.com>
2023-12-03 19:11:16 +0800

    添加docker部署，添加说明

---
+++ src/main/java/com/example/reggie_waimai/config/UrlConfig.java
@@ -0,0 +1,10 @@
+//package com.example.reggie_waimai.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@Configuration
+//@RequestMapping("/api") // 添加全局的请求路径前缀
+//public class UrlConfig {
+//}
